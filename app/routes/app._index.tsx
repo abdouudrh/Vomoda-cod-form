7dac05ef942794f7bcd116c950151003b55486d8
Abdou Driche <abdelrrahman3@gmail.com>
2026-04-11 16:32:54 +0100

    Preserve Shopify query params on app redirect

--- app/routes/app._index.tsx
+++ app/routes/app._index.tsx
@@ -1,6 +1,9 @@
 import type { LoaderFunctionArgs } from "react-router";
 import { redirect } from "react-router";
 
-export const loader = async (_args: LoaderFunctionArgs) => {
-  return redirect("/app/facebook-pixel");
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  const search = url.search || "";
+
+  return redirect(`/app/facebook-pixel${search}`);
 };
